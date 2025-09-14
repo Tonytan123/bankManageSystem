c360adc796531fe3f75fd88bd917a603ca77ef75
tonytan233 <tonytancanbebetter@gmail.com>
2025-09-14 15:23:35 +0800

    修改pom代码

--- src/main/java/com/org/bank/manage/system/controller/BankController.java
+++ src/main/java/com/org/bank/manage/system/controller/BankController.java
@@ -7,6 +7,8 @@ import com.org.bank.manage.system.request.DeleteBankAccountVO;
 import com.org.bank.manage.system.request.UpdateBankAccountVO;
 import com.org.bank.manage.system.service.BankManageService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -23,35 +25,35 @@ public class BankController {
 
     @PostMapping("/createBankAccount")
     @ResponseBody
-    public BaseResponse<?> createBankAccount(@Valid @Validated @RequestBody CreateBankAccountVO req){
+    public BaseResponse<?> createBankAccount(@Valid @Validated @RequestBody @NotNull CreateBankAccountVO req){
         return this.bankManageService.createBankAccount(req);
     }
 
     @PostMapping("/updateBankAccount")
     @ResponseBody
-    public BaseResponse<?> updateBankAccount(@Valid @Validated @RequestBody UpdateBankAccountVO req){
+    public BaseResponse<?> updateBankAccount(@Valid @Validated @RequestBody @NotNull UpdateBankAccountVO req){
         return this.bankManageService.updateBankAccount(req);
     }
 
     @PostMapping("/deleteBankAccount")
-    public BaseResponse<?> updateBankAccount(@Valid @Validated @RequestBody DeleteBankAccountVO req){
+    public BaseResponse<?> updateBankAccount(@Valid @Validated @RequestBody @NotNull DeleteBankAccountVO req){
         return this.bankManageService.deleteBankAccount(req);
     }
 
     @GetMapping("/getBankAccountPage/userId/{userId}")
-    public BaseResponse<?> getBankAccountPage(@PathVariable("userId") String userId,
-                                              @RequestParam("pageNo") Integer pageNo,
-                                              @RequestParam("pageSize") Integer pageSize){
+    public BaseResponse<?> getBankAccountPage(@PathVariable("userId") @NotNull String userId,
+                                              @RequestParam("pageNo") @NotNull Integer pageNo,
+                                              @RequestParam("pageSize") @NotNull Integer pageSize){
         return this.bankManageService.listAccount(pageNo, pageSize, userId);
     }
 
     @GetMapping("/getBankAccountDetail/bankCardNumber/{bankCardNumber}")
-    public BaseResponse<?> getBankAccountDetail(@PathVariable("bankCardNumber") String bankCardNumber){
+    public BaseResponse<?> getBankAccountDetail(@PathVariable("bankCardNumber") @NotNull String bankCardNumber){
         return this.bankManageService.detailAccount(bankCardNumber);
     }
 
     @PostMapping("/bankTransfer")
-    public BaseResponse<?> bankTransfer(@Valid @Validated @RequestBody BankTransferVO bankTransferVO){
+    public BaseResponse<?> bankTransfer(@Valid @Validated @RequestBody @NotNull BankTransferVO bankTransferVO){
         return this.bankManageService.bankTransfer(bankTransferVO);
     }
 }
